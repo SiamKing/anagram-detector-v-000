aa105c86ecf50f1b3ab51a04584a60356f4fce92
SiamKing <thesiameseelectric@gmail.com>
2016-03-18 10:51:22 -0700

    Anagrammed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_split = word.split("")
+    array.select {|i| word_split.sort == i.split("").sort}
+
+  end
+
+
+end
