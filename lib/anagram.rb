cd3962d24e13b282cda9d56d3024686d2568b2ca
SiamKing <thesiameseelectric@gmail.com>
2018-10-19 11:29:42 -0700

    Fun

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        matching = []
+        arr.each do |word|
+            if (word.split('').sort === @word.split('').sort)
+               matching << word
+            end
+        end
+        matching
+    end
+
+end
